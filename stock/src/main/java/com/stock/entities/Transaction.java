1d8ab763797c47f593b54e1c452542dedcfdddef
Aishwarya <aishwaryabali2019@gmail.com>
2026-09-01 00:01:51 -0400

    Made Transaction Entity.

--- stock/src/main/java/com/stock/entities/Transaction.java
+++ stock/src/main/java/com/stock/entities/Transaction.java
@@ -1,5 +1,45 @@
 package com.stock.entities;
 
-public class Transaction {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="transaction")
+public class Transaction {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="TransactionID")
+	private int transactionID;
+	@Column(name="OrderDateTime")
+	private LocalDate orderDate;
+	@Column(name="Status")
+	private String status;
+	@Column(name="Transaction")
+	private String transaction;
+	@Column(name="Quantity")
+	private int quantity;
+	@Column(name="PurchasePrice")
+	private double purchasePrice;
+	@Column(name="Action_")
+	private String action;
+	@Column(name="UserID")
+	private int userID;
+	@Column(name="CompanyTicker")
+	private String companyTicker;
 }
